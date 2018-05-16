f8064e6522d5a2be5dd0bc6566c9dda349c248d2
Amber Caldwell <acaldwell710@gmail.com>
2018-05-16 00:01:42 -0400

    added changes to myqueue and mystack

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -4,14 +4,27 @@ class MyStack
   def initialize
     @stack = Array.new
     self.top = nil
+    @num = -1
   end
 
   def push(item)
+    @stack.top = @stack.push(item)
   end
 
   def pop
+    temp = @top
+    @stack[@num] = nil
+    @stack.top = @stack.pop(item)
+    @num -= 1
+    @top = @stack[@num]
+    return temp
   end
 
   def empty?
+    if !@queue.empty?
+      @queue.last
+    else
+      true
+    end
   end
 end
