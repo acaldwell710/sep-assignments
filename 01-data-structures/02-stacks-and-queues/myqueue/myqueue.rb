d3d0bc1887a62f46eee19c6baa371b19c43373f4
Amber Caldwell <acaldwell710@gmail.com>
2018-05-17 17:04:35 -0400

    changes to myqueue and mystack

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,22 @@ class MyQueue
   end
 
   def enqueue(element)
+    if @head != @queue[0]
+    		@queue += @queue(element)
+    else
+    		@head
+    end
   end
 
   def dequeue
+    @queue.delete_if { |i| i == 0 }
   end
 
   def empty?
+    if @head == nil
+      return true
+    else
+      return false
+    end
   end
 end
