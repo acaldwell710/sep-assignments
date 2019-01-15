cb180d0c76867b53c6dcd693c82832194ff3b2c9
Amber Caldwell <acaldwell710@gmail.com>
2019-01-15 18:00:15 -0500

    updating open addressing

--- 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
+++ 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
@@ -7,58 +7,70 @@ class OpenAddressing
   end
 
   def []=(key, value)
-    key = index
-    while key = @items[index] && value = @items[index]
-      index += 1
-      if @items.last
-        resize
-        []=(key, value)
-      end
-    end
-    @items[index]
+    #set method
+
+    #temp = Hash.new(key, value)
+    #while(@items[i] != nil && @items[i].key != key)
+    #  i = (i + 1) % size
+    #  if(@items[i] = nil)
+    #    @items[i].value
+    #  else
+    #    resize
+    #  end
+    #  @items[i] = temp
+    #end
   end
 
   def [](key)
-    key = @items[index]
-    @items[index] = head
-    if head != nil
-      while next != nil
-        if key = @items[i].key
-          @items.value
-        end
-        @items[i] = @items[i] + 1
+    #retrieve / get
+
+    key = @items[i]
+    while(@items[i] != nil)
+      if(@items[i].key = key)
+        @items[i].value
+      else
+        nil
       end
     end
+    i = (i + 1) % size
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
-    key.split("")
+    #read hashes pt.1 for calculation, .ord or .sum for ascii
+
+    key.sum % size
   end
 
   # Given an index, find the next open index in @items
-  def next_open_index(index)
-    if @items[index] <= 4
-      @items[index]
-    else @items[index] = 5
-      @items[index] + 1
+  def next_open_index(slot)
+
+    i = hash(slot) % size
+    while(@items[i] != nil && @items[i].slot != slot)
+      if @items[i] != nil
+        i += 1
+      else
+        slot = @items[i]
+      end
+      i = (i + 1) % size
     end
+    i
   end
 
   # Simple method to return the number of items in the hash
   def size
-    @items.count
+    @items.length
   end
 
   # Resize the hash
   def resize
-    new_size = size*2
-    new_items = Array.new(new_size)
+    #new_size = (size * 2)
+    #new_items = Array.new(new_size)
 
-    if max_load_factor > 0.7
-      resize
-    end
+    #if @max_load_factor > 0.7
+    #  resize
+    #end
   end
 end
